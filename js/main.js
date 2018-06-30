585cd2b14a3b8a42e88bc3963955bea3fed5bd74
Aggie Koko <agata.kosior57@gmail.com>
2018-06-30 13:04:30 +0200

    sw registration

--- js/main.js
+++ js/main.js
@@ -181,12 +181,12 @@ createRestaurantHTML = (restaurant) => {
 
 /*
 * Service Worker registration
-// */
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('./sw.js')
-//   .then(function(reg) {
-//     console.log('Registration succeeded. Scope is ' + reg.scope);
-//   }).catch(function(error) {
-//     console.log('Registration failed with ' + error);
-//   });
-// }
+*/
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js')
+  .then(function(reg) {
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch(function(error) {
+    console.log('Registration failed with ' + error);
+  });
+}
